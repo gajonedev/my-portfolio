959684ec4eb12564290c9c534d4931d041041608
Néhémie Gandonou <gajonedev@gmail.com>
2025-08-21 12:47:48 +0100

    update layout metadata and add favicon and Open Graph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Lexend, Outfit } from "next/font/google";
+import { Outfit } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
@@ -7,13 +7,53 @@ import "./globals.css";
 import { ThemeProvider } from "./provider";
 import { cn } from "@/lib/utils";
 
-const lexend = Lexend({ subsets: ["latin"], variable: "--font-lexend" });
-
 const outfit = Outfit({ subsets: ["latin"], variable: "--font-outfit" });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Néhémie Gandonou | Développeur web fullstack et codeur passioné",
+  description:
+    "Développeur web et mobile spécialisé en code et no-code. Je vous construit votre prochaine application. Découvrez mes projets et ma méthodologie.",
+  keywords: [
+    "Next.js",
+    "React",
+    "Node.js",
+    "Développeur Web",
+    "Portfolio",
+    "Gajone",
+    "Gajone Dev",
+    "Néhémie",
+    "Néhémie Gandonou",
+  ],
+  authors: [{ name: "Néhémie Gandonou", url: "https://gajone.me" }],
+  creator: "Néhémie Gandonou",
+  publisher: "Néhémie Gandonou",
+  metadataBase: new URL("https://gajone.me"),
+  openGraph: {
+    title: "Néhémie Gandonou | Développeur web passionné.",
+    description:
+      "Découvrez mes projets, mon approche de travail et mes compétences en développement.",
+    url: "https://gajone.me",
+    siteName: "Portfolio Néhémie",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "fr_FR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Portfolio de Néhémie",
+    description: "Développeur Full-Stack passionné..",
+    images: ["/og-image.png"],
+  },
+  // icons: {
+  //   icon: "/favicon.ico",
+  //   apple: "/favicon.ico",
+  // },
 };
 
 export default function RootLayout({
@@ -23,7 +63,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn(lexend.className, outfit.className, "font-outfit")}>
+      <body className={cn(outfit.className, "font-outfit")}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
